4538fa4beb2ae186a62b292b2b9eb8ffd2d078f1
uriapete <uriarteangelo@gmail.com>
2023-07-08 14:47:34 -0700

    settings - connect user_acc app, set AUTH_USER_MODEL to user_accounts.UserAcc

--- tavlia_backend_project/settings.py
+++ tavlia_backend_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'user_accounts',
 ]
 
 MIDDLEWARE = [
@@ -80,6 +81,8 @@ DATABASES = {
     }
 }
 
+# Our new, custom User Model
+AUTH_USER_MODEL="user_accounts.UserAccount"
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
