1d135e296731b923791628e16f6d1c53345368e0
uriapete <uriarteangelo@gmail.com>
2023-07-08 14:45:40 -0700

    user_accounts/models - create new UserAccount class

--- user_accounts/models.py
+++ user_accounts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class UserAccount(AbstractUser):
+    def __str__(self):
+        return self.username
+    
