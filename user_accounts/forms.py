4a244ab555089ad3e1eaf4c08e492f08339048bd
uriapete <uriarteangelo@gmail.com>
2023-07-08 15:34:35 -0700

    user_acc/forms - add CustomUser forms

---
+++ user_accounts/forms.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+from .models import CustomUser
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model=CustomUser
+        fields = UserCreationForm.Meta.fields
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model=CustomUser
+        fields = UserCreationForm.Meta.fields
