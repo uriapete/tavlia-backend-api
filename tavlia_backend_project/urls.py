401376efbe6b50ec0d8739035df114115a7ba6ac
uriapete <uriarteangelo@gmail.com>
2023-07-17 15:42:45 -0700

    root/urls - include user api in urls

--- tavlia_backend_project/urls.py
+++ tavlia_backend_project/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from rest_framework import routers
+from rest_framework.authtoken import views
 from user_accounts.views import UserViewSet
 
 router=routers.DefaultRouter()
@@ -24,6 +25,7 @@ router.register(r'users',UserViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/',include('rest_framework.urls')),
-    path('',include(router.urls))
+    path('token-auth/',views.obtain_auth_token),
+    path('',include('rest_framework.urls')),
+    path('',include(router.urls)),
 ]
