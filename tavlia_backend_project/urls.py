7fcfc2ea18771f22fd2f67bba6ef4ae21d04a9bf
uriapete <uriarteangelo@gmail.com>
2023-07-17 12:36:28 -0700

    rootproj/urls - fix users url using wrong module name

--- tavlia_backend_project/urls.py
+++ tavlia_backend_project/urls.py
@@ -20,5 +20,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/',include('rest_framework.urls')),
-    path('users/',include('user_account.urls')),
+    path('users/',include('user_accounts.urls')),
 ]
