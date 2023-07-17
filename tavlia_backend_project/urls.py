8f93b19ee5daf7d9597f8ec548c5f5a1357c3bfb
uriapete <uriarteangelo@gmail.com>
2023-07-17 12:43:29 -0700

    move router urls to root urls

--- tavlia_backend_project/urls.py
+++ tavlia_backend_project/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from rest_framework import routers
+from user_accounts.views import UserViewSet
+
+router=routers.DefaultRouter()
+router.register(r'users',UserViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/',include('rest_framework.urls')),
-    path('users/',include('user_accounts.urls')),
+    path('',include(router.urls))
 ]
--- user_accounts/urls.py
+++ user_accounts/urls.py
@@ -1,11 +1,6 @@
 from django.urls import path,include
 from . import views
-from rest_framework import routers
-from .views import UserViewSet
-    
-router=routers.DefaultRouter()
-router.register(r'users',UserViewSet)
 
 urlpatterns = [
-    path('',include(router.urls)),
+    
 ]
