2ccab5a33d98bf9e3c2278ba21d8bdf29925be5b
uriapete <uriarteangelo@gmail.com>
2023-07-18 02:56:25 -0700

    save_files/views.py - implement new view permission

--- save_files/views.py
+++ save_files/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.http import HttpRequest,HttpResponse,HttpResponseRedirect,JsonResponse
 # from django.views.decorators.csrf import csrf_exempt
 from rest_framework import viewsets,permissions
+from .permissions import IsObjUser
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.response import Response
 from .models import SaveFile
@@ -15,7 +16,7 @@ class SaveFileViewSet(viewsets.ModelViewSet):
     queryset=SaveFile.objects.all().order_by("last_updated")
     serializer_class=SaveFileSerializer
     authentication_classes=[TokenAuthentication]
-    permission_classes=[permissions.IsAuthenticated]
+    permission_classes=[IsObjUser]
     
     def create(self, req:HttpRequest):
         reqbod=json.loads(req.body)
