38c4890161531fae9e052637bf7e4949f8ba671d
uriapete <uriarteangelo@gmail.com>
2023-07-18 00:44:36 -0700

    save_files/views.py - create savefile viewset

--- save_files/views.py
+++ save_files/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpRequest,HttpResponse,JsonResponse
+# from django.views.decorators.csrf import csrf_exempt
+from rest_framework import viewsets,permissions
+from rest_framework.parsers import JSONParser
+from .models import SaveFile
+from .serializers import SaveFileSerializer
 
 # Create your views here.
+
+class SaveFileViewSet(viewsets.ModelViewSet):
+    queryset=SaveFile.objects.all().order_by("last_updated")
+    serializer_class=SaveFileSerializer
+    permission_classes=[permissions.IsAuthenticated]
