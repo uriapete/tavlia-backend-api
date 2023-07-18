51bf3a9dd41bd70e01c248c64ae03b9493c76fa3
uriapete <uriarteangelo@gmail.com>
2023-07-18 01:23:31 -0700

    save_files/models.py - change flags and loc to integers

--- save_files/models.py
+++ save_files/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class SaveFile(models.Model):
-    campaign_data=models.TextField(null=False,blank=False)
-    current_location=models.TextField(null=True)
+    flags=models.IntegerField(null=True)
+    current_location=models.IntegerField(null=True)
     user=models.ForeignKey("user_accounts.CustomUser",on_delete=models.CASCADE,)
     created_on=models.DateTimeField(auto_now_add=True)
     last_updated=models.DateTimeField(auto_now=True)
