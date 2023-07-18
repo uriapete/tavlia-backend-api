f0659fb4daaa92d1e4fcc5621274b32142de9954
uriapete <uriarteangelo@gmail.com>
2023-07-18 11:28:42 -0700

    save_files/models.py - add default to savefile model

--- save_files/models.py
+++ save_files/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class SaveFile(models.Model):
-    flags=models.IntegerField(null=True)
-    current_location=models.IntegerField(null=True)
+    flags=models.IntegerField(null=True,default=0)
+    current_location=models.IntegerField(null=True,default=0)
     user=models.ForeignKey("user_accounts.CustomUser",on_delete=models.CASCADE,)
     created_on=models.DateTimeField(auto_now_add=True)
     last_updated=models.DateTimeField(auto_now=True)
