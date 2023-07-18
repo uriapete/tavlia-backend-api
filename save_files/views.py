8bcfeea9a2025f7bab7808470cdf36b5979b32ff
uriapete <uriarteangelo@gmail.com>
2023-07-18 02:38:48 -0700

    save_files/views.py - create create method

--- save_files/views.py
+++ save_files/views.py
@@ -1,14 +1,28 @@
+import json
 from django.shortcuts import render
-from django.http import HttpRequest,HttpResponse,JsonResponse
+from django.http import HttpRequest,HttpResponse,HttpResponseRedirect,JsonResponse
 # from django.views.decorators.csrf import csrf_exempt
 from rest_framework import viewsets,permissions
-from rest_framework.parsers import JSONParser
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.response import Response
 from .models import SaveFile
 from .serializers import SaveFileSerializer
 
 # Create your views here.
 
 class SaveFileViewSet(viewsets.ModelViewSet):
+    
     queryset=SaveFile.objects.all().order_by("last_updated")
     serializer_class=SaveFileSerializer
+    authentication_classes=[TokenAuthentication]
     permission_classes=[permissions.IsAuthenticated]
+    
+    def create(self, req:HttpRequest):
+        reqbod=json.loads(req.body)
+        data=dict(reqbod)
+        data["user"]=req.user.pk
+        serializerdata=SaveFileSerializer(data=data)
+        if(serializerdata.is_valid()):
+            new_save:SaveFile=serializerdata.save()
+            return HttpResponseRedirect(redirect_to=f"{new_save.pk}/")
+            
