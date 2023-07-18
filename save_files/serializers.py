e6a6d2dc85463e04704f3b4490c29c4dba884672
uriapete <uriarteangelo@gmail.com>
2023-07-18 01:24:00 -0700

    save_files/serializers - change fields

--- save_files/serializers.py
+++ save_files/serializers.py
@@ -4,4 +4,4 @@ from .models import SaveFile
 class SaveFileSerializer(serializers.ModelSerializer):
     class Meta:
         model=SaveFile
-        fields=['campaign_data','current_location','user','created_on','last_updated']
+        fields=['flags','current_location','user','created_on','last_updated']
