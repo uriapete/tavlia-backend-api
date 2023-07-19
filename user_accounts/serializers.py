55d45c6673bec79d1a18d97621ed491157641b32
uriapete <uriarteangelo@gmail.com>
2023-07-19 00:28:14 -0700

    user_accounts/serializers.py - add proper permissions for create

--- user_accounts/serializers.py
+++ user_accounts/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework import serializers
+from rest_framework import serializers,permissions
 from .models import CustomUser
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -7,6 +7,7 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         fields=['url','username','password',]
         
     def create(self, validated_data):
+        permission_classes=[permissions.AllowAny]
         user=super().create(validated_data)
         user.set_password(validated_data['password'])
         user.save()
