1aa00dbc08c082035f7bbdad5ad69ee57759b426
uriapete <uriarteangelo@gmail.com>
2023-07-18 21:14:56 -0700

    user_accounts/admin.py - remove looping thru users for generating tokens

--- user_accounts/admin.py
+++ user_accounts/admin.py
@@ -19,9 +19,6 @@ from rest_framework.authtoken.models import Token
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-for user in CustomUser.objects.all():
-    Token.objects.get_or_create(user=user)
-
 @receiver(post_save,sender=settings.AUTH_USER_MODEL)
 def create_auth_token(sender,instance=None,created=False,**kwargs):
     if created:
