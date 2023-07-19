52c19b80a34c58bf11eb0fc35ed1ff7c5dde0f37
uriapete <uriarteangelo@gmail.com>
2023-07-19 00:28:45 -0700

    user_accounts/views.py - remove unused imports in views.py

--- user_accounts/views.py
+++ user_accounts/views.py
@@ -1,10 +1,7 @@
-import json
 from django.http import HttpRequest, JsonResponse
-from django.shortcuts import render
 from .models import CustomUser
 from rest_framework import viewsets
 from rest_framework.views import APIView
-from rest_framework.authtoken.models import Token
 from .serializers import UserSerializer
 
 
