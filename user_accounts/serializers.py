a3f0830788a76237fb353ca33c417e331772caca
uriapete <uriarteangelo@gmail.com>
2023-07-18 23:01:19 -0700

    user_accounts/serializers.py - create create route

--- user_accounts/serializers.py
+++ user_accounts/serializers.py
@@ -4,4 +4,10 @@ from .models import CustomUser
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model=CustomUser
-        fields=['url','username']
+        fields=['url','username','password',]
+        
+    def create(self, validated_data):
+        user=super().create(validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
